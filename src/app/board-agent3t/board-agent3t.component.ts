f4a9e43db340d1577774566da9179045ba7c691b
Laabidi-Wafa <laabidiwafa199@gmail.com>
2021-06-25 22:09:32 +0100

    final version'

--- src/app/board-agent3t/board-agent3t.component.ts
+++ src/app/board-agent3t/board-agent3t.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'app/_services/user.service';
-import { Observable } from 'rxjs';
-import { Demande } from 'app/board-employe/Demande';
-import { DemandeService } from 'app/_services/demande.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Utilisateur } from 'app/board-admin/Utilisateur';
-import { Demandecartegrise } from 'app/board-employe/board-demande/demandecartegrise';
-import { AuthService } from 'app/_services/auth.service';
-import { Demandepermis } from 'app/board-employe/board-demande/demandepermis';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "app/_services/user.service";
+import { Observable } from "rxjs";
+import { Demande } from "app/board-employe/Demande";
+import { DemandeService } from "app/_services/demande.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Utilisateur } from "app/board-admin/Utilisateur";
+import { Demandecartegrise } from "app/board-employe/board-demande/demandecartegrise";
+import { AuthService } from "app/_services/auth.service";
+import { Demandepermis } from "app/board-employe/board-demande/demandepermis";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 @Component({
-  selector: 'app-board-agent3t',
-  templateUrl: './board-agent3t.component.html',
-  styleUrls: ['./board-agent3t.component.css']
+  selector: "app-board-agent3t",
+  templateUrl: "./board-agent3t.component.html",
+  styleUrls: ["./board-agent3t.component.css"],
 })
 export class BoardAgent3tComponent implements OnInit {
   id: number;
@@ -22,102 +22,115 @@ export class BoardAgent3tComponent implements OnInit {
   submitted: false;
   demandeConge: boolean = false;
   isLoggedIn = false;
-  demande: Observable<Demandecartegrise[]>
+  demande: Observable<Demandecartegrise[]>;
   offre: any;
-  demandeFiche: Observable<Demandepermis[]>
-  utilisateurEmp: Observable <Utilisateur>;
+  demandeFiche: Observable<Demandepermis[]>;
+  utilisateurEmp: Observable<Utilisateur>;
   utilisateur: any[];
   listOfPoint: any = [];
   nomofpoint: any;
   pagesize = 4;
   page = 4;
-  demandeconge: boolean =false;
-  vd: boolean =false;
-  display: boolean =false;
-  date: Date ;
+  demandeconge: boolean = false;
+  vd: boolean = false;
+  display: boolean = false;
+  date: Date;
   demf: Demandepermis;
   datasize: any;
   showNotif: boolean = false;
   shownotifpaix: boolean = false;
   message: any;
   pointpost: any;
-  errorMessage = '';
+  errorMessage = "";
   selectedValue: String;
-  closeResult = '';
-  model   = {option: 'option3'};
+  closeResult = "";
+  model = { option: "option3" };
   demandev: Demandecartegrise;
   demv: string;
   point1: any;
   popup2: boolean = false;
   test: any;
-  constructor(private userService: UserService ,private modalService: NgbModal,
-    private auth: AuthService , 
-     private route: ActivatedRoute, private demandeservise: DemandeService, 
-     private router: Router) {
-    }
-    goconge() {
-  this.demandeConge = true;
-  console.log('gocongé', this.demandeConge);
-
-}
+  namecitoyen: any;
+  prenomcitoyen: any;
+  constructor(
+    private userService: UserService,
+    private modalService: NgbModal,
+    private auth: AuthService,
+    private route: ActivatedRoute,
+    private demandeservise: DemandeService,
+    private router: Router
+  ) {}
+  goconge() {
+    this.demandeConge = true;
+    console.log("gocongé", this.demandeConge);
+  }
   reloadData() {
     this.goconge();
-    console.log('test0', this.demandeConge);
+    console.log("test0", this.demandeConge);
     this.demande = this.demandeservise.getMesDemandes();
-     this.demandeservise.getMesDemandes().subscribe(
-      data => {
+    this.demandeservise.getMesDemandes().subscribe(
+      (data) => {
         this.offre = data;
         console.log(this.offre);
-        for ( var i = 0 ; i < this.offre.length; i++ ) { if ( this.offre[i].etat === 'NonValider') {
-          this.showNotif = true; console.log(this.showNotif) }
-    }
+        for (var i = 0; i < this.offre.length; i++) {
+          if (this.offre[i].etat === "NonValider") {
+            console.log("demande citoyen", this.offre[i].citoyen.username);
+            this.namecitoyen = this.offre[i].citoyen.username;
+            console.log("nom", this.namecitoyen);
+
+            console.log("demande citoyen", this.offre[i].citoyen.prenom);
+            this.prenomcitoyen = this.offre[i].citoyen.prenom;
+            console.log("nom", this.prenomcitoyen);
+            this.showNotif = true;
+            console.log(this.showNotif);
+          }
+        }
       },
-      err => {
+      (err) => {
         this.offre = JSON.parse(err.error).message;
       }
     );
-    }
+  }
   reloadDataEmp() {
-     this.userService.getEmpList().subscribe(
-    data => {
-      this.utilisateur = data;
-      console.log(this.utilisateur);
-    },
-    err => {
-      this.utilisateur = JSON.parse(err.error).message;
-    }
-  );
-}
-gofiche() {
-  this.demandeConge = false;
-  console.log('gofiche', this.demandeConge);
-
-}
-reloadfishe() {
-  this.gofiche();
-  console.log('test1' ,this.gofiche())
- this.demandeFiche = this.demandeservise.getMesDemandesf();
- console.log(this.demandeFiche);
-  this.demandeservise.getMesDemandesf().subscribe(
-      data => {
-        console.log('fichedepaix', data);
-        this.offre = data;
-        console.log(this.offre);
-        for ( var i=0 ; i < this.offre.length ; i++ ) {
-          if ( this.offre[i].etat === 'NonValider')
-         { console.log( 'test',this.offre[i].etat) ;
-          this.shownotifpaix = true ;   console.log(this.shownotifpaix);}
-         }
+    this.userService.getEmpList().subscribe(
+      (data) => {
+        this.utilisateur = data;
+        console.log(this.utilisateur);
+      },
+      (err) => {
+        this.utilisateur = JSON.parse(err.error).message;
       }
-);
-}
+    );
+  }
+  gofiche() {
+    this.demandeConge = false;
+    console.log("gofiche", this.demandeConge);
+  }
+  reloadfishe() {
+    this.gofiche();
+    console.log("test1", this.gofiche());
+    this.demandeFiche = this.demandeservise.getMesDemandesf();
+    console.log(this.demandeFiche);
+    this.demandeservise.getMesDemandesf().subscribe((data) => {
+      console.log("fichedepaix", data);
+      this.offre = data;
+      console.log(this.offre);
+      for (var i = 0; i < this.offre.length; i++) {
+        if (this.offre[i].etat === "NonValider") {
+          console.log("test", this.offre[i].etat);
+          this.shownotifpaix = true;
+          console.log(this.shownotifpaix);
+        }
+      }
+    });
+  }
   ngOnInit() {
     this.userService.getProprietaireBoard().subscribe(
-      data => {
+      (data) => {
         this.content = data;
         console.log(this.content);
       },
-      err => {
+      (err) => {
         this.content = JSON.parse(err.error).message;
       }
     );
@@ -130,97 +143,104 @@ reloadfishe() {
     this.aff();
   }
   updateEmployer(id: number) {
-    this.router.navigate(['updateEmp', id]);
+    this.router.navigate(["updateEmp", id]);
   }
   Deleteuser(id: number) {
-    this.userService.deleteuser(id)
-    .subscribe(
-      data => {
+    this.userService.deleteuser(id).subscribe(
+      (data) => {
         console.log(data);
         this.reloadData();
       },
-      error => console.log(error)
-
-      );
-}
-notif() {
-   this.demandeservise.Affichernotification().subscribe(
-  data => {
-    this.message  = data;
-   console.log(this.message);
-  },
-  err => {
-    this.message  = JSON.parse(err.error).message;
+      (error) => console.log(error)
+    );
   }
-);
-}
-affId() {
-   this.auth.affierid().subscribe(
-      data => {
+  notif() {
+    this.demandeservise.Affichernotification().subscribe(
+      (data) => {
+        this.message = data;
+        console.log(this.message);
+      },
+      (err) => {
+        this.message = JSON.parse(err.error).message;
+      }
+    );
+  }
+  affId() {
+    this.auth.affierid().subscribe(
+      (data) => {
         this.proprietaireID = data;
         console.log(this.proprietaireID);
         this.listOfPoint.push(this.proprietaireID);
-        console.log('testpro', this.listOfPoint)
-        for( var i = 0 ; i < this.listOfPoint.length; i++) {
-          this. nomofpoint = this.listOfPoint[i].username;
-          console.log('pro connecter', this.nomofpoint );
-          console.log('pro connecter 2', this.listOfPoint[i]);
+        console.log("testpro", this.listOfPoint);
+        for (var i = 0; i < this.listOfPoint.length; i++) {
+          this.nomofpoint = this.listOfPoint[i].username;
+          console.log("pro connecter", this.nomofpoint);
+          console.log("pro connecter 2", this.listOfPoint[i]);
         }
-        console.log('userpointer', this.listOfPoint);
-        this.test = {'id':'1' , 'username': 'yasmine', 'prenom':'amara' , 'date':'13-02-2020' 
-        ,'email':'amara14@gmail.' ,'password':'ghddks' ,'sexe':'m' ,'tel':'21332415','role_id':'1'};
+        console.log("userpointer", this.listOfPoint);
+        this.test = {
+          id: "1",
+          username: "yasmine",
+          prenom: "amara",
+          date: "13-02-2020",
+          email: "amara14@gmail.",
+          password: "ghddks",
+          sexe: "m",
+          tel: "21332415",
+          role_id: "1",
+        };
       },
-      err => {
+      (err) => {
         this.content = JSON.parse(err.error).message;
       }
     );
-}
-Validation(id: number) {
-  this.demandeConge = false;
-  console.log('testcongé', this.demandeConge)
- this.router.navigate(['detaildemande', this.demandeConge, id]);
- }
-valider(id: number) {
-  this.demandeConge = true;
-  console.log('testfiche', this.demandeConge)
-this.router.navigate(['demandepermis', this.demandeConge, id]);
- }
+  }
+  Validation(id: number) {
+    this.demandeConge = false;
+    console.log("testcongé", this.demandeConge);
+    this.router.navigate(["detaildemande", this.demandeConge, id]);
+  }
+  valider(id: number) {
+    this.demandeConge = true;
+    console.log("testfiche", this.demandeConge);
+    this.router.navigate(["demandepermis", this.demandeConge, id]);
+  }
 
   affdemandecongé() {
     this.demandev = new Demandecartegrise();
-    this.id = this.route.snapshot.params['id'];
-    this.demandeservise.getDemandes(this.id)
-      .subscribe(data => {
-        console.log(data)
+    this.id = this.route.snapshot.params["id"];
+    this.demandeservise.getDemandes(this.id).subscribe(
+      (data) => {
+        console.log(data);
         this.demandev = data;
         this.display = true;
-        console.log('display',  this.display)
-      }, error => console.log(error));
+        console.log("display", this.display);
+      },
+      (error) => console.log(error)
+    );
   }
   aff(): void {
     this.demf = new Demandepermis();
-    this.id = this.route.snapshot.params['id'];
-    this.demandeservise.getDemandesf(this.id)
-      .subscribe(data => {
-        console.log(data)
+    this.id = this.route.snapshot.params["id"];
+    this.demandeservise.getDemandesf(this.id).subscribe(
+      (data) => {
+        console.log(data);
         this.demf = data;
         this.demandeconge = true;
-        console.log('demandeConge fi lfichedepaix',  this.demandeconge)
-      }, error => console.log(error));
+        console.log("demandeConge fi lfichedepaix", this.demandeconge);
+      },
+      (error) => console.log(error)
+    );
   }
 
   comfirmer() {
     this.date = new Date();
     console.log(this.date);
-    this.demandeservise.validerF(this.id, this.demf)
-    .subscribe(
-      data =>  console.log(data),
-      error => console.log(error));
-    this.demf =  new Demandepermis();
+    this.demandeservise.validerF(this.id, this.demf).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
+    this.demf = new Demandepermis();
     console.log(this.demf);
-
+  }
 }
-}
-
-
-
--- src/app/board-agent3t/detail-demande/detail-demande.component.ts
+++ src/app/board-agent3t/detail-demande/detail-demande.component.ts
@@ -1,111 +1,127 @@
-import { Component, OnInit } from '@angular/core';
-import { Demande } from 'app/board-employe/Demande';
-import { ActivatedRoute, Router } from '@angular/router';
-import { DemandeService } from 'app/_services/demande.service';
-import {  Demandecartegrise } from 'app/board-employe/board-demande/demandecartegrise';
-import { Observable } from 'rxjs';
-import { Demandepermis } from 'app/board-employe/board-demande/demandepermis';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { Demande } from "app/board-employe/Demande";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DemandeService } from "app/_services/demande.service";
+import { Demandecartegrise } from "app/board-employe/board-demande/demandecartegrise";
+import { Observable } from "rxjs";
+import { Demandepermis } from "app/board-employe/board-demande/demandepermis";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-detail-demande',
-  templateUrl: './detail-demande.component.html',
-  styleUrls: ['./detail-demande.component.css']
+  selector: "app-detail-demande",
+  templateUrl: "./detail-demande.component.html",
+  styleUrls: ["./detail-demande.component.css"],
 })
 export class DetailDemandeComponent implements OnInit {
- demv: string;
- id: number;
- demande:  Demandecartegrise;
- demandecarteg: boolean = false;
- vd: boolean =false;
- display: boolean =false;
- date: Date ;
- demf: Demandepermis;
- closeResult = '';
- content2: string;
- model   = {option: 'option3'};
-  constructor(private route: ActivatedRoute, private modalService: NgbModal,
-    private demandeservise: DemandeService, private router: Router) { }
+  demv: string;
+  id: number;
+  demande: Demandecartegrise;
+  demandecarteg: boolean = false;
+  vd: boolean = false;
+  display: boolean = false;
+  date: Date;
+  demf: Demandepermis;
+  closeResult = "";
+  content2: string;
+  model = { option: "option3" };
+  constructor(
+    private route: ActivatedRoute,
+    private modalService: NgbModal,
+    private demandeservise: DemandeService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    console.log('testdemande cartegris',  this.demandecarteg)
-    this.demande = new  Demandecartegrise();
-    this.id = this.route.snapshot.params['id'];
-    this.demandeservise.getDemandes(this.id)
-      .subscribe(data => {
-        console.log(data)
+    console.log("testdemande cartegris", this.demandecarteg);
+    this.demande = new Demandecartegrise();
+    this.id = this.route.snapshot.params["id"];
+    this.demandeservise.getDemandes(this.id).subscribe(
+      (data) => {
+        console.log(data);
         this.demande = data;
         this.demandecarteg === true;
         this.display = true;
-        console.log('test4html',  this.display)
-        console.log('testdemandecartegrise',  this.demandecarteg)
-      }, error => console.log(error));
-      this.demandecarteg === true;
-      this.aff();
+        console.log("test4html", this.display);
+        console.log("testdemandecartegrise", this.demandecarteg);
+      },
+      (error) => console.log(error)
+    );
+    this.demandecarteg === true;
+    this.aff();
   }
   aff(): void {
     this.demandecarteg === true;
-      this.demf = new Demandepermis();
-      this.demandecarteg === true;
-      this.id = this.route.snapshot.params['id'];
-      this.demandeservise.getDemandesf(this.id)
-        .subscribe(data => {
-          console.log(data)
-          this.demandecarteg === true;
-          this.demf = data;
-          console.log('testdemande permis',  this.demandecarteg);
-        }, error => console.log(error));
+    this.demf = new Demandepermis();
+    this.demandecarteg === true;
+    this.id = this.route.snapshot.params["id"];
+    this.demandeservise.getDemandesf(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.demandecarteg === true;
+        this.demf = data;
+        console.log("testdemande permis", this.demandecarteg);
+      },
+      (error) => console.log(error)
+    );
   }
   Etatvalider() {
-  if (this.display === true) {
-    console.log('demandeCartegrise', this.display );
-    this.valider();
-  } else {
-    console.log('demandecartegris', this.display);
-     this.comfirmer();
-}
-//this.id = this.route.snapshot.params['date_fiche'];
-//console.log(this.id)
+    if (this.display === true) {
+      console.log("demandeCartegrise", this.display);
+      this.valider();
+    } else {
+      console.log("demandecartegris", this.display);
+      this.comfirmer();
+    }
+
+    //this.id = this.route.snapshot.params['date_fiche'];
+    //console.log(this.id)
   }
   open(content) {
-   this.closeResult = this.demv;
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-      this.closeResult = `Closed with: ${this.demv}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReasons(reason)}`;
-    });
+    this.closeResult = this.demv;
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+          this.closeResult = `Closed with: ${this.demv}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReasons(reason)}`;
+        }
+      );
   }
   private getDismissReasons(reason: any): string {
-   if (reason === ModalDismissReasons.ESC) {
-     return 'by pressing ESC';
-   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-     return 'by clicking on a backdrop';
-   } else {
-     return `with: ${reason}`;
-   }
- }
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   valider() {
     this.date = new Date();
     console.log(this.date);
-    this.demandeservise.validerMedDemandes(this.id, this.demande)
-      .subscribe(data => console.log(data), error => console.log(error));
-    this.demande = new  Demandecartegrise();
+    this.demandeservise.validerMedDemandes(this.id, this.demande).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
+    this.demande = new Demandecartegrise();
     this.gotolist();
     window.location.reload();
   }
   comfirmer() {
     this.date = new Date();
     console.log(this.date);
-    this.demandeservise.validerF(this.id, this.demf)
-    .subscribe(
-      data =>  console.log(data),
-      error => console.log(error));
-    this.demf =  new Demandepermis();
+    this.demandeservise.validerF(this.id, this.demf).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
+    this.demf = new Demandepermis();
     console.log(this.demf);
     this.gotolist();
+  }
+  gotolist() {
+    this.router.navigate(["/pro"]);
+  }
 }
-gotolist() {
-    this.router.navigate(['/pro']);
-  }
-  }
